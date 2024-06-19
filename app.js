01dddd396c8e233d3720b54593179ebad8c1c23f
libertador92 <miralizadenicat@gmail.com>
2024-06-19 05:30:33 -0400

    js homework3

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = 'Mustafa';
+let surname = 'Kamal';
+let nickname = 'Ataturk';
+let fullName = name + ' ' + surname + ' ' + nickname;
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.length-2);
+console.log(fullName.includes('e'));
+console.log(fullName.startsWith('code'));
+console.log(fullName.endsWith('code'));
+console.log(fullName.replaceAll('t','###'));
